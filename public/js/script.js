bd89c5ea2c89aa9b1674cd18f180f59e65013a66
Diego Díaz <kbyteec@gmail.com>
2025-10-30 16:54:24 -0500

    final

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,10 @@
-document.getElementById('btnBuscar').addEventListener('click', () => {
-      const toastElement = document.getElementById('liveToast');
-      const toast = new bootstrap.Toast(toastElement);
-      toast.show();
-    });
+
+
+
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  const tooltipList = [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el));
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    [...popoverTriggerList].map(el => new bootstrap.Popover(el));
+  });
